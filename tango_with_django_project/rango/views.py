fb4bc14f837ebcd57e1ceb3849ad66647588bb94
Chris <chris.karlsson@yahoo.de>
2019-04-23 14:39:24 +0200

    added templates, static and media

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    #First constructing a dictionary with context for the index.html to render utilizing Django template variables
+    #Second return a rendered response to the client (Note 'rango/index.html' indicates the html we want to retrn
+    context_dict = {'boldmessage' : 'this is pretty bold of me'}
+
+    return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page!" + "<a href='/rango/'> MainPage </a>")
